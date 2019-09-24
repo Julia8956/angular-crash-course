d184f2e90b2eb698c8571e938478d2e1035f4a19
julia8956 <minji6654@gmail.com>
2019-09-24 18:10:22 +0900

    final commit

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../../models/Todo';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todos',
@@ -9,27 +10,28 @@ import { Todo } from '../../models/Todo';
 export class TodosComponent implements OnInit {
   todos:Todo[];
 
+  constructor(private todoService: TodoService) {
 
-  constructor() { }
-
-  ngOnInit() {
-    this.todos = [
-      {
-        id: 1,
-        title: 'Todo one',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Todo two',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'Todo three',
-        completed: false
-      }
-    ]
   }
 
+  ngOnInit() {
+    this.todoService.getTodos().subscribe(todos =>{
+      this.todos = todos;
+    });
+  }
+
+  deleteTodo(todo:Todo) {
+    //remove from UI
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    //console.log('delete me');
+    
+    //remove from server
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+
+  addTodo(todo: Todo) {
+    this.todoService.addTodo(todo).subscribe(todo => {
+      this.todos.push(todo);
+    });
+  }
 }
